2fa0d475b16265b031f73e75127f5bed9ed0ce6f
lia-oliveira <dev.gillian@gmail.com>
2025-04-19 18:48:29 -0300

    Basic project

---
+++ client_registration/src/main/java/com/oliveiralia/client_registration/ClientRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiralia.client_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegistrationApplication.class, args);
+	}
+
+}
---
+++ client_registration/src/test/java/com/oliveiralia/client_registration/ClientRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiralia.client_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
